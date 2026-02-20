c9e8f697db63daacf617614609bcf6b85d2978e3
linuxer77 <harshitg187@gmail.com>
2026-02-20 23:20:41 +0530

    models setup

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from core.models import Item
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = "__all__"
+# from core.models import Item
+#
+#
+# class ItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Item
+#         fields = "__all__"
--- core/models.py
+++ core/models.py
@@ -1,7 +1,48 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 
 # Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=200)
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(max_length=200, unique=True)
+    password = models.CharField(max_length=128)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField(max_length=200)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
